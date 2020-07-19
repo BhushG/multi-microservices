781c43b6a1392eae70d594b08dd36db9698087a0
bgosavi <bgosavi@qualys.com>
2020-07-19 11:17:05 +0530

    Service discovery established with consul

--- rating-data-service/src/test/java/com/javabrains/RatingDataServiceApplicationTests.java
+++ rating-data-service/src/test/java/com/javabrains/RatingDataServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.javabrains;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.testng.annotations.AfterTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
